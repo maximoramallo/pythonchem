0559b94d8c06ac1f514e50fe53184afe147cff21
Mat086 <104285200+Mat086@users.noreply.github.com>
2023-02-21 19:21:44 -0300

    Update ejercicio_01.py

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -8,3 +8,15 @@ Un día que el viento soplaba con fuerza...
 - Lo que se mueve es el viento -respondió otro monje.
 - Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro.'''
 
+#Una forma
+caracter = "#"
+lista_frases = []
+while (caracter in texto) == True:
+    linea = texto[:texto.find(caracter)]
+    lista_frases.append(linea)
+    texto = texto[texto.find(caracter)+1:]
+print(texto2[:texto2.find(caracter)].capitalize()+"...")
+for f in lista_frases:
+    if texto2[:texto2.find(caracter)] != f:
+        print("- "+f.capitalize()+".")
+print("- "+texto2[texto2.rfind(caracter)+1:].capitalize()+".")
