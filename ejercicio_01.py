6d9c931606ee6f0cf457bb5281b74e5865c0e4d8
Mat086 <104285200+Mat086@users.noreply.github.com>
2023-02-21 00:35:36 -0300

    Create ejercicio_01.py

---
+++ ejercicio_01.py
@@ -0,0 +1,10 @@
+'''1) Utilizando el manejo de cadenas, listas, sus métodos internos... Transforma este texto:
+un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro
+
+En este otro:
+
+Un día que el viento soplaba con fuerza...
+- Mira como se mueve aquella banderola -dijo un monje.
+- Lo que se mueve es el viento -respondió otro monje.
+- Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro.'''
+
